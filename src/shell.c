8ebb339e99f170c45fe9748c50c920b5079f9ff2
cctv180 <cctv180@gmail.com>
2020-09-19 16:14:37 +0800

    修改shellParserParam函数防止指针越界

--- src/shell.c
+++ src/shell.c
@@ -872,6 +872,11 @@ static void shellParserParam(Shell *shell)
             record = 1;
         }
     }
+	//TODO 防止shell->parser.param[]越界 自动截断多余计数
+	if (shell->parser.paramCount > SHELL_PARAMETER_MAX_NUMBER)
+	{
+		shell->parser.paramCount = SHELL_PARAMETER_MAX_NUMBER;
+	}
 }
 
 
@@ -883,12 +888,6 @@ static void shellParserParam(Shell *shell)
 static void shellRemoveParamQuotes(Shell *shell)
 {
     unsigned short paramLength;
-    //TODO 防止shell->parser.param[]越界 自动截断多余参数
-    if (shell->parser.paramCount > SHELL_PARAMETER_MAX_NUMBER)
-    {
-        shell->parser.paramCount = SHELL_PARAMETER_MAX_NUMBER;
-    }
-
     for (unsigned short i = 0; i < shell->parser.paramCount; i++)
     {
         if (shell->parser.param[i][0] == '\"')
