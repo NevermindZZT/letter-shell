2e7ec71c5c38ed59d38d204c6c3dd92bb9a1d84e
Letter <nevermindzzt@gmail.com>
2021-07-31 13:39:27 +0800

    新增 命令行游戏demo

---
+++ extensions/game/game.c
@@ -0,0 +1,21 @@
+/**
+ * @file game.c
+ * @author Letter
+ * @brief game
+ * @version 0.1
+ * @date 2021-07-18
+ * 
+ * @copyright (c) 2021 Letter
+ * 
+ */
+#include "shell_cmd_group.h"
+
+extern int main_2048(int argc, char *argv[]);
+
+ShellCommand gameGroup[] =
+{
+    SHELL_CMD_GROUP_ITEM(SHELL_TYPE_CMD_MAIN, 2048, main_2048, 
+        game 2048\n2048 [param]\nParam: blackwhite bluered or null),
+    SHELL_CMD_GROUP_END()
+};
+SHELL_EXPORT_CMD_GROUP(SHELL_CMD_PERMISSION(0)|SHELL_CMD_TYPE(SHELL_TYPE_CMD_MAIN), game, gameGroup, games);
